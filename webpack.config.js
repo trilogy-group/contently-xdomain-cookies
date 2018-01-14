3a3780b7f1bb0f1fe33f7cd38ac65fcc5827728e
lysk88 <lysk88@gmail.com>
2018-01-14 18:15:28 +0200

    chore(): remove unnecessary webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,8 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: {
-        //'xdomain_cookie': './src/xdomain_cookie.js',
-        //'xdomain_cookie.min': './src/xdomain_cookie.js',
-        'user-cookie-provider': './src/user-cookie-provider.js' //['./src/user-cookie-provider.js',  './src/xdomain_cookie.js']
+        'user-cookie-provider': './src/user-cookie-provider.js'
     },
-    // plugins: [
-    //     new UglifyJSPlugin({
-    //         include: /\.min\.js$/,
-    //         minimize: true
-    //     })
-    // ],
     output: {
         path: __dirname + '/dist', // output directory
         filename: "[name].js" // name of the generated bundle
